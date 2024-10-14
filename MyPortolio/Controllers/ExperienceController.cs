2c0c0b6f288f3c52b52fa1e9a7c2bce553c954b8
Kaan Kaya <kaankaya.0707@gmail.com>
2024-10-14 13:43:43 +0300

    Experience View Eklendi ve Listelendi

--- MyPortolio/Controllers/ExperienceController.cs
+++ MyPortolio/Controllers/ExperienceController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortolio.DAL.Context;
 
 namespace MyPortolio.Controllers
 {
     public class ExperienceController : Controller
     {
+        private readonly MyPortfolioContext _portfolioContext;
+        public ExperienceController(MyPortfolioContext portfolioContext)
+        {
+            _portfolioContext = portfolioContext;
+        }
         public IActionResult ExperinceList()
         {
-            return View();
+            var values = _portfolioContext.Experinces.ToList();
+            return View(values);
         }
     }
 }
